36a1863d6f51e2648e47d6e9b64e3869fc314cc6
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-05 16:43:20 +0300

    ex3

--- server.js
+++ server.js
@@ -3,11 +3,41 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan('dev'));
-
+//ex1
 app.get('/greeting/:name' , function(req,res){
     const name =req.params.name
     res.send(name)
     // console.log(req.query)
+})
+//ex2
+app.get('/roll/:num', function(req,res){
+ const number = Number(req.params.num)
+     
+    console.log(typeof(number))
+    
+    const rand = Math.floor(Math.random() * num);
+    console.log(num)
+      if (typeof(number) === 'number'){
+            res.send(`you rolled a ${rand}`)
+       }else{
+        res.send('this is not a number')
+       }
+
+})
+
+//ex3
+   const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+    app.get('/collectibles/:index' ,function(req,res){  
+     let index = req.params.index
+        if (collectibles [index]){
+            res.send(collectibles[index])
+         } else{
+            res.send('This item is not yet in stock. Check back soon!')
+         }
 })
 
 app.listen(3000, function()
