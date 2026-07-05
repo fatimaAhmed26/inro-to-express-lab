b2200cb388d03c72e904baa35881544b9da3bc6e
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-05 20:32:51 +0300

    the full lab slution

--- server.js
+++ server.js
@@ -41,8 +41,50 @@ app.get('/roll/:num', function(req,res){
             res.send('This item is not yet in stock. Check back soon!')
          }
 })
+app.get('/hello', (req, res) => {
+    res.send(`Hello there, ${req.query.name}! I hear you are ${req.query.age} years old!`);
+});
+
+const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
+
+  //ex 4
+
+app.get('/shoes', function(req, res){
+    const minPrice = req.query['min-price']
+    const maxPrice = req.query['max-price']
+    const type = req.query['type']
+
+    console.log(minPrice)
+    console.log(maxPrice)
+    console.log(type)
+
+    let results = []
+
+    for (let i = 0; i < shoes.length; i++){
+        const shoe = shoes[i]
+
+        if (minPrice && shoe.price < Number(minPrice)){
+        } else if (maxPrice && shoe.price > Number(maxPrice)){
+        } else if (type && shoe.type !== type){
+        } else {
+            results.push(shoe)
+        }
+    }
+    res.send(results)
+})
 
 app.listen(3000, function()
 {
     console.log('listening on port 3000')
 })
+
+
+  
